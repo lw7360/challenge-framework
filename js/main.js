7ec26f0306558c0b2d8d6767db91eaa524fa0319
Larry Wu <lw7360@gmail.com>
2014-10-02 20:05:23 -0700

    Prettified some html and js

--- js/main.js
+++ js/main.js
@@ -4,73 +4,69 @@ editor.getSession().setMode("ace/mode/javascript");
 editor.setShowPrintMargin(false);
 
 challenge.whitelist = {
-  "ForStatement": "Use a for loop",
-  "VariableDeclaration": "Declare a variable"
+    "ForStatement": "Use a for loop",
+    "VariableDeclaration": "Declare a variable"
 };
 
 challenge.blacklist = {
-  "WhileStatement": "Don't use a while loop"
+    "WhileStatement": "Don't use a while loop"
 };
 
 challenge.struct = {
-  "'for loop' and inside of it 'if statement'." : {
-    "ForStatement" : {
-      "IfStatement" : null
+    "'for loop' and inside of it 'if statement'.": {
+        "ForStatement": {
+            "IfStatement": null
+        }
     }
-  }
 };
 
 var listGroupItem = function(txt, pass) {
-  var ok = pass ? "ok" : "remove";
-  var success = pass ? "success" : "danger";
+    var ok = pass ? "ok" : "remove";
+    var success = pass ? "success" : "danger";
 
-  return '<li class="list-group-item"><span class="badge alert-' + success
-            + '"><span class="glyphicon glyphicon-' + ok + '"></span></span>'
-            + txt + '</li>';
+    return '<li class="list-group-item"><span class="badge alert-' + success + '"><span class="glyphicon glyphicon-' + ok + '"></span></span>' + txt + '</li>';
 }
 
 var dispChallenge = function() {
-  try {
-    var parsed = challenge.parse(editor.getValue());
-    //console.log(parsed);
+    try {
+        var parsed = challenge.parse(editor.getValue());
+        //console.log(parsed);
 
-    var success = true;
+        var success = true;
 
-    $("#whitelistStatus").html('');
-    for (prop in parsed.whitelist) {
-      $("#whitelistStatus").append(listGroupItem(challenge.whitelist[prop], parsed.whitelist[prop]));
-      if (!parsed.whitelist[prop]) success = false;
-    }
+        $("#whitelistStatus").html('');
+        for (prop in parsed.whitelist) {
+            $("#whitelistStatus").append(listGroupItem(challenge.whitelist[prop], parsed.whitelist[prop]));
+            if (!parsed.whitelist[prop]) success = false;
+        }
 
-    $("#blacklistStatus").html('');
-    for (prop in parsed.blacklist) {
-      $("#blacklistStatus").append(listGroupItem(challenge.blacklist[prop], parsed.blacklist[prop]));
-      if (!parsed.blacklist[prop]) success = false;
-    }
+        $("#blacklistStatus").html('');
+        for (prop in parsed.blacklist) {
+            $("#blacklistStatus").append(listGroupItem(challenge.blacklist[prop], parsed.blacklist[prop]));
+            if (!parsed.blacklist[prop]) success = false;
+        }
 
-    $("#structStatus").html('');
-    for (prop in parsed.struct) {
-      $("#structStatus").append(listGroupItem(prop, parsed.struct[prop]));
-      if (!parsed.struct[prop]) success = false;
-    }
+        $("#structStatus").html('');
+        for (prop in parsed.struct) {
+            $("#structStatus").append(listGroupItem(prop, parsed.struct[prop]));
+            if (!parsed.struct[prop]) success = false;
+        }
 
-    if (success) {
-      $("#success").html('<a target="_blank" href="http://i.imgur.com/w3PEgSw.gif">You did it!</a>');
+        if (success) {
+            $("#success").html('<a target="_blank" href="http://i.imgur.com/w3PEgSw.gif">You did it!</a>');
+        } else {
+            $("#success").html('Keep on trucking...');
+        }
+    } catch (err) {
+        // console.log(err);
+        $("#success").html("Syntax error...");
     }
-    else {
-      $("#success").html('Keep on trucking...');
-    }
-  }
-  catch (err) {
-    // console.log(err);
-    $("#success").html("Syntax error...");
-  }
 }
 
 editor.getSession().on('change', function(e) {
-  setTimeout(function() {
-    dispChallenge();
-  }, 0);
+    setTimeout(function() {
+        dispChallenge();
+    }, 0);
 });
 
 dispChallenge();
